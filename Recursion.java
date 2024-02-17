4422e0678858cd6f026ad651eb53fdc4bc1478e3
pritam105 <pritammane105@mail.com>
2024-02-17 12:53:03 +0530

    add comments and a reverse array method with void return type

--- Recursion.java
+++ Recursion.java
@@ -4,11 +4,11 @@ public class Recursion {
 
     public static void main(String[] args) {
         int[] arr = array1toN(20);
-        int[] arr1 = {6,7};
+        int[] arr1 = {6, 7};
 //        arr = Arrays.copyOf(arr, 6);
 //        System.arraycopy(arr1, 0, arr, 1, arr1.length);
 //        reverseArr(arr, 0, 4);
-        reverseArr2(arr);
+        reverseArr3(arr);
         System.out.println(Arrays.toString(arr));
 //        Arrays.stream(arr).forEach(System.out::println);
     }
@@ -28,44 +28,69 @@ public class Recursion {
     }
 
     private static int sum(int n) {
-        if(n>0)
-            return n + sum(n-1);
+        if (n > 0)
+            return n + sum(n - 1);
         return n;
     }
 
-    private static int[] array1toN(int x){
-        if(x>1){
+    private static int[] array1toN(int x) {
+        if (x > 1) {
             int[] arr;
-            arr = Arrays.copyOf(array1toN(x-1), x);
-            arr[x-1] = x;
+            arr = Arrays.copyOf(array1toN(x - 1), x);
+            arr[x - 1] = x;
             return arr;
         }
         return new int[]{1};
     }
 
-    private static void reverseArr(int[] arr, int start, int end){
-        if(end-start < 2)
+    /*
+    Reverse an array with recursion. Function modifies the array passed.
+     */
+    private static void reverseArr(int[] arr, int start, int end) {
+        if (end - start < 2)
             return;
         int temp;
         temp = arr[end];
         arr[end] = arr[start];
         arr[start] = temp;
-        reverseArr(arr, start+1, end-1);
+        reverseArr(arr, start + 1, end - 1);
 
     }
 
-    private static int[] reverseArr2(int[] arr){
-        if(arr.length <2)
+    /*
+    Reverse an array with recursion and just the array as argument.
+    Function modifies the array passed and returns the reversed array
+     */
+    private static int[] reverseArr2(int[] arr) {
+        if (arr.length < 2)
             return arr;
         int temp;
         temp = arr[0];
-        arr[0] = arr[arr.length -1];
-        arr[arr.length -1] = temp;
-        int[] reversedArray = reverseArr2(Arrays.copyOfRange(arr, 1, arr.length-1));
+        arr[0] = arr[arr.length - 1];
+        arr[arr.length - 1] = temp;
+        int[] reversedArray = reverseArr2(Arrays.copyOfRange(arr, 1, arr.length - 1));
 
-        if(reversedArray.length!=0)
+        if (reversedArray.length != 0)
             System.arraycopy(reversedArray, 0, arr, 1, reversedArray.length);
 
         return arr;
+    }
+
+    /*
+    Reverse an array with recursion and just the array as argument.
+    Function modifies the array passed with void return type.
+     */
+    private static void reverseArr3(int[] arr) {
+        if (arr.length < 2)
+            return;
+        int temp;
+        temp = arr[0];
+        arr[0] = arr[arr.length - 1];
+        arr[arr.length - 1] = temp;
+        int[] subArr = Arrays.copyOfRange(arr, 1, arr.length - 1);
+        reverseArr3(subArr);
+
+        if (subArr.length != 0)
+            System.arraycopy(subArr, 0, arr, 1, subArr.length);
     }
 }
