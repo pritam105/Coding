443b1a811e8b30f9ee7ac1c7c0b72abe6429ad46
pritam105 <pritammane105@mail.com>
2024-02-19 22:53:23 +0530

    code for second largest in an array

--- ArraysPractice.java
+++ ArraysPractice.java
@@ -7,10 +7,23 @@ public class ArraysPractice {
     public static void main(String[] args){
         int[] arr = PracticeUtils.createArray(6);
         System.out.println(Arrays.toString(arr));
-        System.out.println("Max: "+ secondLargest(arr));
+        System.out.println("Max: "+ secondLargest(new int[]{5,5,4,3,2,1}));
     }
 
     private static int secondLargest(int[] arr){
+        int max = Integer.MIN_VALUE;
+        int max2 = Integer.MIN_VALUE;
 
+        for (int j : arr) {
+            if (j > max) max = j;
+        }
+
+        for(int j : arr){
+            if(j != max && j > max2) max2 = j;
+        }
+
+        if(max2 == Integer.MIN_VALUE) max2 = -1;
+
+        return max2;
     }
 }
