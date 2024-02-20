1534bbb3e84ae52bd9efcc733a195802aa943462
pritam105 <pritammane105@mail.com>
2024-02-20 23:55:32 +0530

    print frequency of elements in an array

--- ArraysPractice.java
+++ ArraysPractice.java
@@ -1,11 +1,14 @@
 import java.sql.SQLOutput;
 import java.util.Arrays;
+import java.util.HashMap;
 
 public class ArraysPractice {
 
 
     public static void main(String[] args){
         int[] arr = PracticeUtils.createArray(6);
+
+        frequency(arr);
         System.out.println(Arrays.toString(arr));
         System.out.println("Max: "+ secondLargest(new int[]{5,5,4,3,2,1}));
     }
@@ -14,6 +17,9 @@ public class ArraysPractice {
         int max = Integer.MIN_VALUE;
         int max2 = Integer.MIN_VALUE;
 
+        /*
+        Find second largest in 2 traversals
+         */
 //        for (int j : arr) {
 //            if (j > max) max = j;
 //        }
@@ -26,6 +32,9 @@ public class ArraysPractice {
 //
 //        return max2;
 
+        /*
+        Find second largest in one traversal
+         */
         for(int i : arr){
             if(i>max){
                 max2 = max;
@@ -34,5 +43,15 @@ public class ArraysPractice {
             else if (i > max2 && i != max) max2 = i;
         }
         return max2;
+    }
+
+    private static void frequency(int[] arr){
+        HashMap<Integer, Integer> frequencyMap = new HashMap<>();
+
+        for(int i : arr){
+            if(frequencyMap.containsKey(i)) frequencyMap.put(i, frequencyMap.get(i) + 1);
+            else frequencyMap.put(i, 1);
+        }
+        System.out.println(frequencyMap.entrySet());
     }
 }
